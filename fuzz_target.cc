2fc3b2b549ea233d9c3fdd649f6da74193a22d86
Vivek Kumar Singh <vschs007@gmail.com>
2017-04-09 10:08:07 +0530

    updated
    space efficient

--- fuzz_target.cc
+++ fuzz_target.cc
@@ -2,10 +2,8 @@
 #include "guetzli/jpeg_data.h"
 #include "guetzli/jpeg_data_reader.h"
 #include "guetzli/processor.h"
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string jpeg_data(reinterpret_cast<const char*>(data), size);
-
   // Ignore large images, to prevent timeouts.
   guetzli::JPEGData jpg_header;
   if (!guetzli::ReadJpeg(data, size, guetzli::JPEG_READ_HEADER, &jpg_header)) {
@@ -15,7 +13,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   if (static_cast<int64_t>(jpg_header.width) * jpg_header.height > kMaxPixels) {
     return 0;
   }
-
   // TODO(robryk): Use nondefault parameters.
   guetzli::Params params;
   std::string jpeg_out;
@@ -23,7 +20,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   // TODO(robryk): Verify output distance if Process() succeeded.
   return 0;
 }
-
-
-
-
